234896d5a7d4e2dafe1407fcf3be2c28c2360016
Naoki Okada <naokiokada11@gmail.com>
2019-09-29 17:04:47 -0400

    Changed structure of directory

---
+++ just-a-start/build/precache-manifest.c60d4b8bd9f6b6bda7a82c91eb7ebe9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c545b44a63d559de25d6b7581d8a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d25ac86e6b4cad54ff42",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "36212995a8b017648ed4",
+    "url": "/static/js/2.cd930777.chunk.js"
+  },
+  {
+    "revision": "d25ac86e6b4cad54ff42",
+    "url": "/static/js/main.dc9953bf.chunk.js"
+  },
+  {
+    "revision": "cb4fd463e7184abd8f6a",
+    "url": "/static/js/runtime~main.d52357dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
