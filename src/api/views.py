8bcf339334fbbe5561dd7daf1bd416d8d82011d8
Sam <meetsammy1007@gmail.com>
2019-10-17 22:01:19 -0400

    progress on views, tests, model

--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your functions here.\
+
+def test_connection(request):
+    return HttpResponse('Connection successful')
+
+def add(request):
+    # do something
+
+def delete(request):
+    # do something
+
+def edit(request):
+    # do something
+
+
