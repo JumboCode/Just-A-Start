d9c1fd95316e2ee2020feae3224218ac7a44dffb
Naoki Okada <naokiokada11@gmail.com>
2019-10-19 14:12:25 -0400

    Added model

--- src/just-a-start/views.py
+++ src/just-a-start/views.py
@@ -7,8 +7,7 @@ from django.conf import settings
 
 class FrontendAppView(View):
     """
-    Serves the compiled frontend entry point (only works if you have run `yarn
-    run build`).
+    Serves the compiled frontend entry point (only works if you have run 'npm run build').
     """
 
     def get(self, request):
@@ -21,7 +20,7 @@ class FrontendAppView(View):
                 """
                 This URL is only used when you have built the production
                 version of the app. Visit http://localhost:3000/ instead, or
-                run `yarn run build` to test the production version.
+                run 'npm run build' to test the production version.
                 """,
                 status=501,
             )
