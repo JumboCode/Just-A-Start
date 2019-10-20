29abbb9d75d00aceec687b566e03f189d8e2a4d5
Mico <theogenemicomyiza@gmail.com>
2019-10-20 18:07:09 -0400

    function to send text to a list of users

--- src/api/views.py
+++ src/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from twilio.rest import Client
 
 # Create your functions here.
 
@@ -17,3 +18,18 @@ def edit(request):
 def index(request):
     my_dict = {'insert_me': "Hello I am from view.py!"}
     return render(request,'index.html', context=my_dict)
+
+def send_text(list_users, text):
+    # Your Account SID from twilio.com/console
+    account_sid = ""
+    # Your Auth Token from twilio.com/console
+    auth_token  = ""
+
+    client = Client(account_sid, auth_token)
+
+    #iterate through list of people
+    for user in list_users:
+        message = client.messages.create(
+            from_="+12034429209", 
+            body= text,
+            to = user)
