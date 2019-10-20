46a2c989667ac2d1229b6d3ae66fc843396493c5
Naoki Okada <naokiokada11@gmail.com>
2019-10-19 20:59:18 -0400

    PostgreSQL setup in progress

--- src/just-a-start/settings.py
+++ src/just-a-start/settings.py
@@ -87,16 +87,16 @@ STATICFILES_DIRS = [
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'justastart',
-#         'USER': 'jas',
-#         'PASSWORD': 'justastart',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgresql',
+        'USER': 'jas',
+        'PASSWORD': 'justastart',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
