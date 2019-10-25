7a58f9351c1067e15559e646ba06f5d25421fee8
Jason Xu <gwangsenshoe@gmail.com>
2019-10-24 22:06:55 -0400

    finished the body of the admin dashboard

---
+++ src/client/src/pages/AdminDashboardBody.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './AdminDashboardBody.css';
+import UserActivityEntry from './UserActivityEntry.jsx';
+import AdminDashboardDropdown from './AdminDashboardDropdown.jsx'
+
+class AdminDashboardBody extends React.Component {
+  render(){
+    return(
+      <body>
+        <div class = "dashboard">
+          <div class = "top_content">
+            <h1>Activity Log</h1>
+            <input type="text" id = "search" />
+            <div class = "container">
+              <div id = "flex_container_two">
+                <AdminDashboardDropdown name = "All Dates"/>
+                <AdminDashboardDropdown name = "All Authors"/>
+                <button id = "filter" type="button">Filter</button>
+              </div>
+              <div id = "flex_container_three">
+                <button class = "filter" id = "left" type="button"> ></button>
+                <input type="text" class = "page_search" />
+                <p id="page_text"> of 5</p>
+                <button class = "filter" type="button"> ></button>
+              </div>
+            </div>
+          </div>
+          <ul class = "flex_container">
+            <li>Date</li>
+            <li>Author</li>
+            <li>Action</li>
+            <li>Description</li>
+          </ul>
+          <UserActivityEntry />
+          <UserActivityEntry />
+          <UserActivityEntry />
+          <UserActivityEntry />
+          <UserActivityEntry />
+          <UserActivityEntry />
+        </div>
+      </body>
+    )
+  }
+}
+
+export default AdminDashboardBody;
---
+++ src/client/src/pages/AdminDashboardDropdown.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './AdminDashboardDropdown.css';
+
+class AdminDashboardDropdown extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div class="custom-select">
+        <select>
+          <option value="0">{this.props.name}</option>
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+          <option value="3">Option 3</option>
+          <option value="4">Option 4</option>
+          <option value="5">Option 5</option>
+          <option value="6">Option 6</option>
+          <option value="7">Option 7</option>
+          <option value="8">Option 8</option>
+          <option value="9">Option 9</option>
+          <option value="10">Option 10</option>
+          <option value="11">Option 11</option>
+          <option value="12">Option 12</option>
+        </select>
+      </div>
+    )
+  }
+}
+
+export default AdminDashboardDropdown;
