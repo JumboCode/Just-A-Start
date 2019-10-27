b34676283563025bd0444ebadf4f97fdc97b9f4a
Naoki Okada <naokiokada11@gmail.com>
2019-10-27 17:44:38 -0400

    Resolved merge conflicts

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -12,7 +12,7 @@ import AdminDashboardBody from './pages/AdminDashboardBody'
 import UserActivityEntry from './pages/UserActivityEntry'
 
 
-ReactDOM.render(<AdminDashboardBody />,document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
