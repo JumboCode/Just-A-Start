f440f5f8e04b9fadae4e15f3dbbe75162f610810
Meg <megunaokawa@gmail.com>
2019-11-11 18:07:24 -0500

    softcode admin user list

--- src/frontend/src/pages/UsersEntry.jsx
+++ src/frontend/src/pages/UsersEntry.jsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './UsersEntry.css';
 import ProfilePic from '../assets/profilepic.png'
+import PropTypes from 'prop-types';
 
 class UserActivityEntry extends React.Component {
   render(){
+    const { data } = this.props;
     return(
       <body>
         <div>
@@ -12,25 +14,47 @@ class UserActivityEntry extends React.Component {
           <img src={ProfilePic} />
           <div class = "flex_container">
             <div class = "item">
-              <p id = "regular" id = "medium">KBrown01</p>
+              <p id = "regular" id = "medium">{data.username}</p>
             </div>
             <div class = "item">
-              <p id = "regular" id = "medium">Kate</p>
+              <p id = "regular" id = "medium">{data.name_first}</p>
             </div>
             <div class = "item">
-              <p id = "regular" id = "medium">Brown</p>
+              <p id = "regular" id = "medium">{data.name_last}</p>
             </div>
             <div class = "item">
-              <p id = "small_margin">katebrown@gmail.com</p>
-              <p id = "small_margin">+1(617)-283-1837</p>
+              <p id = "small_margin">{data.email}</p>
+              <p id = "small_margin">{data.phone}</p>
             </div>
             <div class = "item">
-              <p id = "regular">1 hour ago</p>
+              <p id = "regular">{data.last_login}</p>
             </div>
           </div>
         </div>
       </body>
     )
+  }
+}
+
+UserActivityEntry.propTypes = {
+  data: PropTypes.shape({
+    time: PropTypes.string,
+    name: PropTypes.string,
+    change_type: PropTypes.string,
+    change_desc: PropTypes.string,
+    user_type: PropTypes.string,
+    id: PropTypes.isRequired,
+  })
+}
+
+UserActivityEntry.defaultProps = {
+  data: {
+    time: '',
+    name: '',
+    change_type: '',
+    change_desc: '',
+    user_type: '',
+    id: PropTypes.isRequired,
   }
 }
 
