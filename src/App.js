c5dc3647313e73fa213d50e114f89de66dcbb7c5
Meg <megunaokawa@gmail.com>
2020-02-23 18:45:14 -0500

    set up login functionality on front end

--- src/App.js
+++ src/App.js
@@ -13,11 +13,28 @@ import UserDashboard from './pages/UserDashboard';
 import NotFoundPage from "./pages/404";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false,
+      authToken: "",
+    }
+    this.setAuthToken = this.setAuthToken.bind(this);
+  }
+
+  setAuthToken(token) {
+    console.log(`setting auth token to ${token}`);
+    this.setState({
+      authToken: token,
+      isAuthenticated: true,
+    });
+  }
+
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Login}/>
+          <Route exact path="/" component={() => <Login setAuthToken={this.setAuthToken} />} />
           <Route exact path="/user-dashboard" component={UserDashboard} />
           <Route exact path="/admin-notification" component={AdminNotification} />
           <Route exact path="/admin-userlist" component={AdminUserList} />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-// import ReactDOM from 'react-dom';
 import Navbar from '../components/Navbar.jsx';
 import LoginButton from '../components/LoginButton.jsx';
-import Form from '../components/Form.jsx';
 import CheckBox from '../components/Checkbox.jsx';
 import jas_man from '../assets/jas.man.png';
 import jas_woman from '../assets/jas.woman.png';
@@ -18,9 +16,55 @@ class Login extends Component {
       keepLoggedIn: false
     };
 
+    didCheckBox = () => {
+      this.setState((prevState, props) =>
+        ({keepLoggedIn: (prevState.keepLoggedIn === true) ? false : true}));
+    }
 
-    constructor() {
-        super();
+    didForgetPassword = () => {
+      this.setState((prevState, props) =>
+        ({forgotPassword: true}));
+    }
+
+    retrieveUserInfo = (u, p) => {
+      this.setState({username: u, password: p});
+    }
+
+    handleClick = () => {
+      const { username, password } = this.state;
+      const { setAuthToken } = this.props;
+      this.setState((prevState, props) =>
+        ({isClicked: prevState.isClicked === false ? true : false}));
+
+      const fetchOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token}`
+        },
+        body: JSON.stringify({username, password}),
+      }
+      fetch('http://localhost:8000/api/rest-auth/login/', fetchOptions)
+        .then(res => res.json())
+        .then(res => {
+          setAuthToken(res['key']);
+        })
+        .catch(err => {
+          console.error(err);
+        })
+
+    }
+
+    setFields = () => {
+      this.setState({username: this._un.value, password: this._ps.value});
+    }
+
+    changeUsernameHandler = (event) => {
+      this.setState({username: event.target.value});
+    }
+    changePasswordHandler = (event) => {
+      this.setState({password: event.target.value});
+
     }
 
     render() {
@@ -35,11 +79,39 @@ class Login extends Component {
                 <div id="loginText">
                 </div>
                 <div id="form-login">
-                  <Form getUserInfo={this.retrieveUserInfo} forgotPassword={this.didForgetPassword}
-                  isClicked={this.state.isClicked}/>
+                  <form>
+                    <div>
+                      <input
+                        className="loginInput"
+                        ref={(el) => this._un = el}
+                        type="text"
+                        value={this.state.username}
+                        onChange={this.changeUsernameHandler}
+                        placeholder="Username/Email address">
+                      </input>
+                    </div>
+
+                    <div>
+                      <input
+                      className="loginInput" ref={(el) => this._ps = el}
+                      type="password"
+                      value={this.state.password}
+                      onChange={this.changePasswordHandler}
+                      placeholder="Password">
+                      </input>
+                    </div>
+                    <div>
+                      <span
+                        id="forgotPass"
+                        onClick={() => this.props.forgotPassword()}>
+                      Forgot Password? </span>
+                    </div>
+                  </form>
                 </div>
                 <div id="loginButton-login">
-                  <LoginButton loginClick={this.handleClick} isClicked={this.state.isClicked}/>
+                  <LoginButton
+                    loginClick={this.handleClick}
+                    isClicked={this.state.isClicked}/>
                 </div>
                 <div id="checkbox-login">
                   <CheckBox checked={this.didCheckBox}/>
@@ -49,46 +121,6 @@ class Login extends Component {
             </div>
           </div>
         );
-    }
-
-
-    componentDidUpdate = (prevProps, prevState) => {
-      if (prevState.username != this.state.username) {
-        this.printFields();
-      }
-      if (prevState.forgotPassword != this.state.forgotPassword) {
-        console.log(this.state.forgotPassword);
-      }
-      if (prevState.keepLoggedIn != this.state.keepLoggedIn) {
-        console.log(this.state.keepLoggedIn);
-      }
-    }
-
-    didCheckBox = () => {
-      this.setState((prevState, props) =>
-        ({keepLoggedIn: (prevState.keepLoggedIn == true) ? false : true}));
-    }
-
-    didForgetPassword = () => {
-      this.setState((prevState, props) =>
-        ({forgotPassword: true}));
-    }
-
-    retrieveUserInfo = (u, p) => {
-      this.setState({username: u, password: p});
-    }
-
-    handleClick = () => {
-      console.log("Clicked");
-      this.setState((prevState, props) =>
-        ({isClicked: prevState.isClicked == false ? true : false}));
-      setTimeout(10, this.printFields);
-      this.printFields();
-    }
-
-    printFields = () => {
-      console.log(this.state.username);
-      console.log(this.state.password);
     }
 }
 
