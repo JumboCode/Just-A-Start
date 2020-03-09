3b68000f32da59084d161627a7789466a99f1f41
Naoki Okada <naokiokada11@gmail.com>
2020-03-09 15:25:20 -0400

    Removed venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\blins\documents\projects\jumbocode\just-a-start\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
